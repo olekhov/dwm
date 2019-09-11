5f0d5a5be0c44ae637d6d29283ef8bd909205df5
Vasiliy Olekhov <olekhov@gmail.com>
2019-09-11 19:42:16 +0300

    update

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 //static const char *fonts[]          = { "xos4 Terminus:size=16" };
-static const char *fonts[]          = { "xos4 Terminus:size=12" };
-static const char dmenufont[]       = "xos4 Terminus:size=16";
+static const char *fonts[]          = { "xos4 Terminus:size=20" };
+static const char dmenufont[]       = "xos4 Terminus:size=20";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -25,8 +25,8 @@ static const unsigned int alphas[][3]      = {
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
-static const char* const girl="👩";
-static const char* const boy="👨";
+static const char* const girl="1";
+static const char* const boy="2";
 
 
 static const char *tags[] = { girl, boy , "3", "4", "5", "6", "7", "8", "9" };
