e305422afbfc42931deb3328191155c17c5cce66
Vasiliy Olekhov <olekhov@gmail.com>
2019-09-26 18:59:43 +0300

    update

--- config.h
+++ config.h
@@ -6,8 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 //static const char *fonts[]          = { "xos4 Terminus:size=16" };
-static const char *fonts[]          = { "xos4 Terminus:size=12" };
-static const char dmenufont[]       = "xos4 Terminus:size=16";
+static const char *fonts[]          = { "Terminus-14" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -92,8 +91,6 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]={"st",NULL};
 #include "movestack.c"
 static Key keys[] = {
